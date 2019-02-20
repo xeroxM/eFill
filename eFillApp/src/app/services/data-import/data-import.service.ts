2620b464e511aa69b67bcbaa9bf6ce1fdd21960b
Bene <bene_mildenberger@web.de>
2019-02-21 00:12:36 +0100

    update last changes for merge

--- eFillApp/src/app/services/data-import/data-import.service.ts
+++ eFillApp/src/app/services/data-import/data-import.service.ts
@@ -1,11 +1,10 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Platform} from '@ionic/angular';
 import {SQLitePorter} from '@ionic-native/sqlite-porter/ngx';
 import {SQLite, SQLiteObject} from '@ionic-native/sqlite/ngx';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
 import {Storage} from '@ionic/storage';
-// import {map} from 'rxjs/operators';
 
 interface StationInformation {
     long: number;
@@ -18,7 +17,7 @@ interface StationInformation {
 
 @Injectable()
 export class DataImportService {
-   // private url = 'assets/fake-data/loading_stations.json';
+    // private url = 'assets/fake-data/loading_stations.json';
     database: SQLiteObject;
     public databaseReady: BehaviorSubject<boolean>;
     public stationInformation = [];
@@ -36,7 +35,9 @@ export class DataImportService {
             })
                 .then(async (db: SQLiteObject) => {
                     this.database = db;
-                    await this.database.executeSql('DROP TABLE IF EXISTS loadingstations').then(() => {}).catch(() => {});
+                    await this.database.executeSql('DROP TABLE IF EXISTS loadingstations').then(() => {
+                    }).catch(() => {
+                    });
                     this.storage.set('database_filled', await this.checkTableExists('loadingstations'));
                     console.log('Cleared table');
                     this.storage.get('database_filled').then(async val => {
@@ -50,50 +51,50 @@ export class DataImportService {
         });
     }
 
-   public async fillDatabase() {
+    public async fillDatabase() {
         await this.http.get('assets/data/efillDB.sql', {responseType: 'text' as 'text'}).subscribe(sql => {
-                const time = Date.now();
-                console.log('Starting DB import');
-                this.sqlitePorter.importSqlToDb(this.database._objectInstance, sql)
-                    .then(async () => {
-                        this.databaseReady.next(true);
-                        this.storage.set('database_filled', true);
-                        console.log(`Importing DB took ${Date.now() - time} milliseconds`);
-                        console.log(await this.getAllDBEntries());
-                       // this.stationInformation = await this.getAllDBEntries();
-                        console.log(this.stationInformation);
-                    }).catch(e => console.error(e));
-            });
+            const time = Date.now();
+            console.log('Starting DB import');
+            this.sqlitePorter.importSqlToDb(this.database._objectInstance, sql)
+                .then(async () => {
+                    this.databaseReady.next(true);
+                    this.storage.set('database_filled', true);
+                    console.log(`Importing DB took ${Date.now() - time} milliseconds`);
+                    console.log(await this.getAllDBEntries());
+                }).catch(e => console.error(e));
+        });
     }
+
     /*getCoordinates(): Observable<StationInformation[]> {
         return this.http.get<StationInformation[]>(this.url);
     }*/
 
     public async checkTableExists(tablename) {
-      let res = {
-        rows: {
-          length: 0,
-          item: (i) => {}
-        }
-      };
-      await this.database.executeSql(`SELECT name FROM sqlite_master WHERE type='table' AND name='${tablename}';`)
-                    .then(e => res = e)
-                    .catch(e => res = e);
-      return (res.rows.length > 0) ? true : false;
+        let res = {
+            rows: {
+                length: 0,
+                item: (i) => {
+                }
+            }
+        };
+        await this.database.executeSql(`SELECT name FROM sqlite_master WHERE type='table' AND name='${tablename}';`)
+            .then(e => res = e)
+            .catch(e => res = e);
+        return (res.rows.length > 0);
     }
 
     public async getAllDBEntries() {
-      const temp = [];
-      await this.database.executeSql('SELECT * FROM loadingstations').then(data => {
-        for (let i = 0; i < data.rows.length; i++) {
-          temp.push(data.rows.item(i));
-        }
-      }).catch(data => {
-        for (let i = 0; i < data.rows.length; i++) {
-          temp.push(data.rows.item(i));
-        }
-      });
-      return temp;
-      // console.log(temp, 'Hello');
+        const temp = [];
+        await this.database.executeSql('SELECT * FROM loadingstations').then(data => {
+            for (let i = 0; i < data.rows.length; i++) {
+                temp.push(data.rows.item(i));
+            }
+        }).catch(data => {
+            for (let i = 0; i < data.rows.length; i++) {
+                temp.push(data.rows.item(i));
+            }
+        });
+        return temp;
+        // console.log(temp, 'Hello');
     }
 }
